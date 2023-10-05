0b47d4551658a49a60d45c6464900cb84185210a
DotNero <koryukov021@gmail.com>
2023-10-05 03:21:48 +0300

    config file added, express added

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
 const http = require("http");
 const emitter = require('events');
+const express = require('express');
+const mysql = require('mysql2');
+import config from './config.json';
+
+const app = express();
+//обработчик события "/"
+app.get('/',function(request,response){
+    response.send("<h2>1<h2>")
+})
+
+app.listen(5000)
+
+
+const connection = mysql.createConnection({});
+
+
+
+
+
+
 
 emi = new emitter();
 
@@ -11,6 +31,6 @@ emi.on('hello', function(){
 //создание сервера с использованием модуля http и потоков. 
 //request -- поток для чтения
 //response -- поток для записи
-http.createServer(function(request,response){
+// http.createServer(function(request,response){
 
-}).listen(5000)
+// }).listen(5000)
